960b45377736f10c418ddae74c6d8d94b917b474
Pedro Ricardo <pedroricardo.dev@gmail.com>
2024-09-14 18:01:34 -0300

    inicia o menu da aplicação

--- aula.md
+++ aula.md
@@ -3,3 +3,12 @@
 Coletar os dados
 Processar os dados (manipular, alterar...)
 Apresentar os dados
+
+## Módulos em Node.js:
+
+Importação de módulos (require, ComonJS)
+Biblioteca 'inquirer' para criar prompts interativos
+
+## Programação assíncrona e Promises:
+
+Uso de funções assíncronas (async/await)
---
+++ index.js
@@ -0,0 +1,38 @@
+const {select} = require('@inquirer/prompts');
+
+const start = async() => {
+
+    while(true){
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        });
+
+        switch(opcao) {
+            case "cadastrar":
+                console.log("Vamos cadastrar");
+                break
+            case "listar":
+                console.log("Vamos listar");
+                break
+            case "sair":
+                console.log("Até a próxima!")
+                return
+        }
+    }
+}
+
+start();
