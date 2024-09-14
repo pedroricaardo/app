68ffdc9e410f64c450143b3e2c09384b908d5d66
Pedro Ricardo <pedroricardo.dev@gmail.com>
2024-09-14 18:10:46 -0300

    cadastrar meta

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const {select} = require('@inquirer/prompts');
+const {select, input} = require('@inquirer/prompts');
+
+let metas = [];
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta:"})
+
+    if(meta.length == 0) {
+        console.log("A meta não pode ser vazia.");
+        return
+    }
+
+    metas.push({
+        value: meta,
+        checked: false
+    });
+}
 
 const start = async() => {
 
@@ -24,6 +40,8 @@ const start = async() => {
         switch(opcao) {
             case "cadastrar":
                 console.log("Vamos cadastrar");
+                await cadastrarMeta();
+                console.log(metas);
                 break
             case "listar":
                 console.log("Vamos listar");
