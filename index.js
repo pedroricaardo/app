bfe26c7790b7ef33ae806b4914a86c8d0e488970
Pedro Ricardo <pedroricardo.dev@gmail.com>
2024-09-16 19:48:05 -0300

    deletar metas

--- index.js
+++ index.js
@@ -54,11 +54,54 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas: " + realizadas.length,
         choices: [...realizadas]
     })
 
     //console.log(realizadas);
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas!");
+        return
+    }
+
+    await select({
+        message: "Metas Abertas: " + abertas.length,
+        choices: [...abertas]
+    })
+}
+
+const deletarMetas = async () => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return {value: meta.value,
+                checked: false
+        }
+    })
+
+    const itensADeletar =  await checkbox({
+        message: "Selecione item para deletar:",
+        choices: [...metasDesmarcadas],
+        instructions: false
+    })
+
+    if(itensADeletar.length == 0){
+        console.log("Nenhum item para deletar.");
+        return
+    }
+    
+    itensADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item
+        })
+    })
+
+    console.log("Meta(s) deletada(s) com sucesso!");
 }
 
 const start = async() => {
@@ -80,6 +123,10 @@ const start = async() => {
                     value: "realizadas"
                 },
                 {
+                    name: "Deletar metas",
+                    value: "deletar"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -97,6 +144,12 @@ const start = async() => {
             case "realizadas":
                 await metasRealizadas();
                 break
+            case "abertas":
+                await metasAbertas();
+                break
+            case "deletar":
+                await deletarMetas();
+                break
             case "sair":
                 console.log("Até a próxima!")
                 return
@@ -105,3 +158,5 @@ const start = async() => {
 }
 
 start();
+
+//Parei no Min 13:43 - Aula 3
